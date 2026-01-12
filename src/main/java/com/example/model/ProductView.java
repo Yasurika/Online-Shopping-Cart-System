ec883ad48e406911701e81e9ba5add2dbb557263
poorviwickramasinghe-design <poorviwickramasinghe@gmail.com>
2026-01-12 23:22:50 +0530

    Added all Java backend files

---
+++ src/main/java/com/example/model/ProductView.java
@@ -0,0 +1,19 @@
+src/main/java/com/example/controller/ProductController.java
+src/main/java/com/example/service/ProductService.java
+src/main/java/com/example/model/Product.java
+src/main/java/com/example/model/ProductView.java
+src/main/java/com/example/repository/ProductRepository.java
+src/main/java/com/example/repository/ProductViewRepository.java
+src/main/java/com/example/dto/CartItemRequest.java
+```
+
+### Frontend Files to Upload (Person 3):
+```
+src/main/webapp/pages/products.html
+src/main/webapp/pages/product-detail.html
+src/main/webapp/js/products.js
+src/main/webapp/js/product-search.js
+src/main/webapp/js/product-filter.js
+src/main/webapp/css/products.css
+src/main/webapp/css/product-detail.css
+src/main/webapp/js/category.js
